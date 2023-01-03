ab76f8b0365b3a2906b8d0c7022d651d781fa5c1
NickIvanov <nikolas-bmx@yandex.ru>
2023-01-03 23:07:30 +0400

    Added my project

---
+++ infrastructure/Shop.Memory/CarRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Shop.Memory
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly Car[] cars = new[]
+        {
+            new Car(1, "Nissan"),
+            new Car(2, "Toyota"),
+            new Car(3, "Lada"),
+        };
+        public Car[] GetByModel(string modelPart)
+        {
+            return cars.Where(car => car.Model.Contains(modelPart)).ToArray();
+        }
+    }
+}
---
+++ presentation/Shop.Web/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ICarRepository carRepository;
+            public SearchController (ICarRepository carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+        public IActionResult Index(string query)
+        {
+            var cars = carRepository.GetByModel(query);
+            return View(cars);
+        }
+    }
+}
