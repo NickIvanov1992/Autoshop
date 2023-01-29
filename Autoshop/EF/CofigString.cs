cf8da47e6a91f5cd8ed6b7622844a2b09c6c6710
NickIvanov1992 <nikolas-bmx@mail.ru>
2023-01-29 20:46:29 +0400

    Finally

--- Autoshop/EF/CofigString.cs
+++ Autoshop/EF/CofigString.cs
@@ -1,14 +1,14 @@
-﻿using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
-// незадействован
-namespace Store.EF
-{
-    public class CofigString
-    {
-        private IConfigurationRoot configurationRoot;
-        public CofigString(IHostingEnvironment hostingEnvironment)
-        {
-            configurationRoot = new ConfigurationBuilder().SetBasePath(hostingEnvironment.ContentRootPath)
-                .AddJsonFile("dbsettings.json").Build();
-        }
-    }
-}
+﻿//using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+//// незадействован
+//namespace Store.EF
+//{
+//    public class CofigString
+//    {
+//        private IConfigurationRoot configurationRoot;
+//        public CofigString(IHostingEnvironment hostingEnvironment)
+//        {
+//            configurationRoot = new ConfigurationBuilder().SetBasePath(hostingEnvironment.ContentRootPath)
+//                .AddJsonFile("dbsettings.json").Build();
+//        }
+//    }
+//}
